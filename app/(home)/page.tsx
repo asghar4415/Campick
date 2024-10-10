357abc7a00a60c4084b8c2b664ddfa12ba5effb4
Asghar Ali <asgharali.224415@gmail.com>
2024-10-10 11:51:55 +0500

    updated

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'CamPick',
+  description: 'Home Page'
+};
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
